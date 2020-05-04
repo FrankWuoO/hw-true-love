001dc61de879a8c07d365cccf605bf96f1ecca1b
cheng-en wu <>
2020-05-04 14:37:39 +0800

    setup project

---
+++ HWTrueLove/HWTrueLove/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  HWTrueLove
+//
+//  Created by cheng-en wu on 5/4/20.
+//  Copyright © 2020 cheng-en wu. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
